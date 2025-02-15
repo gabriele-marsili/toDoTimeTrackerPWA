82a87ff760b46668552d9bccffae93dc8242dcf6
WhoIsMars <marsiligabri@gmail.com>
2025-02-15 17:02:40 +0100

    first commit - project initialization

---
+++ src/backend-comunication/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = { // TODO
+    apiKey: "TUA_API_KEY",
+    authDomain: "TUO_PROGETTO.firebaseapp.com",
+    projectId: "TUO_PROJECT_ID",
+    storageBucket: "TUO_STORAGE_BUCKET",
+    messagingSenderId: "TUO_MESSAGING_SENDER_ID",
+    appId: "TUO_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ vite.config.ts
@@ -0,0 +1,45 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "ToDo TimeTracker PWA",
+        short_name: "TTT",
+        description: "A toDo list and time tracker PWA",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/firestore.googleapis.com/,
+            handler: "NetworkFirst",
+            options: {
+              cacheName: "firebase-cache",
+            },
+          },
+        ],
+      },
+      devOptions:{enabled:true}
+    }),
+  ],
+})
